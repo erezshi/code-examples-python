e03286c5c98d9745dc1115b00f2011dd3660e2fb
erezshi <erez.shi@gmail.com>
2020-03-23 17:16:42 +0200

    Create betterpython.py

---
+++ betterpython.py
@@ -0,0 +1,33 @@
+#ternary condition
+condition = True
+x = 1 if condition else x = 0
+
+#Large numbers
+ternary condition
+n1 = 10_000_000_000
+n2 = 100_000_000
+total = n1+n2
+print(f'{total:,}')
+10,100,000,000
+
+#enumerate - display the position of the value in the list
+names = ['David', 'Fisher', 'Davidson', 'Yoda']
+or index, name in enumerate(names, start=1):
+  print(index, name)
+
+1 David
+2 Fisher
+3 Davidson
+4 Yoda
+
+# unpacking multiple lists
+python unpack multiple lists together 
+titles = ['Mr', 'Ms', 'Mrs', 'Mr']
+names = ['David', 'Diana', 'Mor', 'Joe']
+for name, title in zip(names, titles):
+   print(f'Hey {title} {name} nice to meet you')
+
+Hey Mr David nice to meet you
+Hey Ms Diana nice to meet you
+Hey Mrs Mor nice to meet you
+Hey Mr Joe nice to meet you
